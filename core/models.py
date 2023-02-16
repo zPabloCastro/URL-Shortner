24f78fc0754290af23674668837b0000e56224cf
zPabloCastro <zpablocastro@gmail.com>
2023-02-15 22:51:22 -0300

    first commit

---
+++ core/models.py
@@ -0,0 +1,11 @@
+from core import db, app
+from datetime import datetime
+
+class ShortUrls(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    original_url = db.Column(db.String(500), nullable=False)
+    short_id = db.Column(db.String(20), nullable=False, unique=True)
+    created_at = db.Column(db.DateTime, default=datetime.now(), nullable=False)
+
+with  app.app_context():
+    db.create_all()
